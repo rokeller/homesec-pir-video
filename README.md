eb44368b1a800937fa70666d22b35b80a3480832
Roger Keller <rokeller@cymbeline.ch>
2018-02-11 17:09:28 +0100

    Initial draft.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # homesec-pir-video
-Simple golan app for RaspberryPi to read a signal from a PIR sensor and record a video for the duration of the signal.
+
+Simple golang app for RaspberryPi to read a signal from a PIR sensor and record a video for the duration of the signal.
+
+## What's this
+
+This simple app listens for signal change in the PIR data. When the PIR sensor signals HIGH, the app starts recording (using `raspivid`), and it stops recording when the signal goes back to LOW (or when the app terminates). Videos are created with a resolution of 720x480 and the default h264 encoding at 25 fps. Video files are stored under `/data/video` (make sure the directory exists before starting the app), and the files are named using the timestamp from when the recording started.
+
+You'll likely need to run with `sudo`, since I haven't gotten around to make sure it works as normal user (in the gpio group). You can also run it from root with cron, e.g. with a `crontab` entry:
+
+`@reboot /path/to/homesec-pir-video`
+
+Since raspivid creates raw h264 files, you must play the video with the FPS explicitly set to 25, e.g. with mplayer:
+
+`mplayer -fps 20180211_162256.h264`
+
+## Wiring
+
+Wire the data channel of the PIR sensor to GPIO 4 (pin 7 in the physical numbering, see <https://www.raspberrypi.org/documentation/usage/gpio/>), or change the code accordingly to use a different GPIO pin.
---
+++ pir-video.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+type Direction int
+
+const (
+	In Direction = iota
+	Out
+)
+
+type Pin struct {
+	Id      int
+	Dir     Direction
+	pinPath string
+	fdDir   *os.File
+}
+
+func export(id int) {
+	err := ioutil.WriteFile("/sys/class/gpio/export", []byte(strconv.Itoa(id)), 0666)
+
+	if nil != err {
+		log.Panicf("Failed to export Pin %d: %v", id, err)
+	}
+}
+
+func unexport(id int) {
+	err := ioutil.WriteFile("/sys/class/gpio/unexport", []byte(strconv.Itoa(id)), 0666)
+
+	if nil != err {
+		log.Panicf("Failed to unexport Pin %d: %v", id, err)
+	}
+}
+
+func OpenPin(id int, dir Direction) *Pin {
+	export(id)
+
+	pinPath := fmt.Sprintf("/sys/class/gpio/gpio%d", id)
+	fdDir, err := os.OpenFile(pinPath+"/direction", os.O_WRONLY|os.O_SYNC, 0666)
+
+	if nil != err {
+		log.Panicf("Failed to open direction file for Pin %d: %v", id, err)
+	}
+
+	var dirString string
+
+	switch dir {
+	case In:
+		dirString = "in"
+
+	case Out:
+		dirString = "out"
+	}
+
+	_, err = fdDir.Write([]byte(dirString))
+
+	if nil != err {
+		log.Panicf("Failed to set direction of Pin %d to '%s': %v", id, dirString, err)
+	}
+
+	return &Pin{
+		Id:      id,
+		Dir:     dir,
+		pinPath: pinPath,
+		fdDir:   fdDir,
+	}
+}
+
+func (pin *Pin) Read() bool {
+	fdValue, err := os.OpenFile(pin.pinPath+"/value", os.O_RDONLY|os.O_SYNC, 0666)
+
+	if nil != err {
+		log.Panicf("Failed to open value file for Pin %d: %v", pin.Id, err)
+	}
+
+	defer fdValue.Close()
+
+	buf := make([]byte, 4)
+
+	for {
+		read, err := fdValue.Read(buf)
+
+		if nil != err {
+			if io.EOF != err {
+				log.Panicf("Failed to read from Pin %d: %v", pin.Id, err)
+			}
+		} else if 1 <= read {
+			break
+		}
+	}
+
+	return buf[0] == '1'
+}
+
+func (pin *Pin) Close() {
+	pin.fdDir.Close()
+
+	unexport(pin.Id)
+}
+
+func startRecording() *exec.Cmd {
+	path := fmt.Sprintf("/data/video/%s.h264", time.Now().Format("20060102_150405"))
+	cmd := exec.Command("raspivid", "-t", "0", "-o", path, "-w", "720", "-h", "480", "-fps", "25", "-b", "250000")
+	cmd.Start()
+
+	return cmd
+}
+
+func recorder(commands <-chan string) {
+	var cmdRecord *exec.Cmd = nil
+
+	for {
+		select {
+		case cmd := <-commands:
+			switch cmd {
+			case "start":
+				log.Println("Start recording.")
+				cmdRecord = startRecording()
+
+			case "stop":
+				log.Println("Stop recording.")
+
+				if nil != cmdRecord {
+					cmdRecord.Process.Signal(syscall.SIGINT)
+				}
+
+			default:
+				log.Printf("Unsupported command: %s", cmd)
+			}
+		}
+	}
+}
+
+func checkPIR(pin *Pin, signals <-chan os.Signal, done chan<- bool, commands chan<- string) {
+	lastHigh := false
+
+	for {
+		select {
+		case sig := <-signals:
+			log.Printf("Got signal: %v", sig)
+
+			// If we're currently recording, stop the recording.
+			if lastHigh {
+				commands <- "stop"
+			}
+
+			done <- true
+			break
+
+		default:
+			curHigh := pin.Read()
+
+			if curHigh != lastHigh {
+				if lastHigh {
+					commands <- "stop"
+				} else {
+					commands <- "start"
+				}
+
+				lastHigh = curHigh
+			}
+
+			time.Sleep(time.Millisecond * 250)
+		}
+	}
+}
+
+func main() {
+	signals := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	commands := make(chan string, 1)
+
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	fmt.Println("PIR-controlled Video Recording Controller")
+
+	pirIn := OpenPin(4, In)
+	defer pirIn.Close()
+
+	go recorder(commands)
+	go checkPIR(pirIn, signals, done, commands)
+
+	<-done
+}
